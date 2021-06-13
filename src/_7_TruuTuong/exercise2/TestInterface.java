2c3d5c03b56b5a52bd1406eafae180e992a57c8b
unknown <nhatdv510@wru.vn>
2021-06-13 20:18:30 +0700

    up

--- src/_7_TruuTuong/exercise2/TestInterface.java
+++ src/_7_TruuTuong/exercise2/TestInterface.java
@@ -5,10 +5,16 @@ public class TestInterface {
         Shape[] shapes = new Shape[3];
         shapes[0] = new Square(5.0);
         shapes[1] = new Rectangle(6.0,7.5);
+        shapes[2] = new Circle(3.5);
+        System.out.println("Aria "+((Circle)shapes[2]).getArea());
+        System.out.println("Aria "+((Rectangle)shapes[1]).getAria());
+        System.out.println("Aria "+((Square)shapes[0]).getAria());
 
-//         for (Shape element : shapes) {
-//             System.out.println(element.g);
-//             if (element instanceof )
-//         }
+        for (int i = 0 ; i<shapes.length;i++) {
+            if (shapes[i] instanceof Colorable) {
+                System.out.println("it is Square ");
+                ((Colorable)shapes[i]).howToColor();
+            }
+        }
     }
 }
